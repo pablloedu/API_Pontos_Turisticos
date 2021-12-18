8772ea1763573730f257eedef679fd55aa7e20b1
pablloedu <pablloedu@gmail.com>
2021-12-18 11:20:53 -0300

    filtro get_queryset

--- core/API/viewsets.py
+++ core/API/viewsets.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -6,4 +7,16 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     serializer_class = PontoTuristicoSerializer
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+        if nome:
+            queryset = PontoTuristico.objects.filter(nome=nome)
+        if descricao:
+            queryset = PontoTuristico.objects.filter(descricao=descricao)
+
+        return queryset
